45067562d44648be20e8e8fec1f68fbdc8b75925
Firdaus Banjar <agam5599@gmail.com>
2024-03-21 11:18:49 +0800

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
 	return (
 		<header className="px-8 py-5 bg-black text-white">
 			<Link href="/threads">
-				<h1 className="text-4xl font-bold">{'Forum-Application'}</h1>
+				<h1 className="text-4xl font-bold">{'Forum-App'}</h1>
 			</Link>
 		</header>
 	);
